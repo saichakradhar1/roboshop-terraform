f2af10e9cebf7849afb95c96e05bb58391cc20e5
saichakradhar <saichakradhar1427@gmail.com>
2023-06-29 14:28:16 +0530

    Creating Instances

--- servers.tf
+++ servers.tf
@@ -7,6 +7,10 @@ resource "aws_instance" "frontend" {
   }
 }
 
+output "frontend"{
+  value = aws_instance.frontend.public_ip
+}
+
 resource "aws_instance" "Mongodb" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.small"
