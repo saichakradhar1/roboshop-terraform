8668bf1e2e0a6e220cb7331a289466f2697689f6
saichakradhar <saichakradhar1427@gmail.com>
2023-07-03 14:31:30 +0530

    Creating Dns records

--- servers.tf
+++ servers.tf
@@ -4,12 +4,12 @@ data "aws_ami" "centos" {
   name_regex  = "Centos-8-DevOps-Practice"
 }
 
-output "ami"{
+output "ami" {
   value = data.aws_ami.centos.image_id
 }
 
 resource "aws_instance" "frontend" {
-  ami           = "data.aws_ami.centos.image_id"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t2.small"
 
   tags = {
@@ -17,20 +17,20 @@ resource "aws_instance" "frontend" {
   }
 }
 
-output "frontend"{
+output "frontend" {
   value = aws_instance.frontend.public_ip
 }
 
-#resource "aws_route53_record" "frontend" {
-#  zone_id = "Z08622982W6HMKSZCC7DQ"
-#  name    = "frontend-dev.rdevopsb72.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.frontend.private_ip]
-#}
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z08622982W6HMKSZCC7DQ"
+  name    = "frontend-dev.rdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
 
 resource "aws_instance" "mongodb" {
-  ami           = "data.aws_ami.centos.image_id"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t2.small"
 
   tags = {
@@ -38,16 +38,16 @@ resource "aws_instance" "mongodb" {
   }
 }
 
-#resource "aws_route53_record" "mongodb" {
-#  zone_id = "Z08622982W6HMKSZCC7DQ"
-#  name    = "mongodb-dev.rdevopsb72.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.mongodb.private_ip]
-#}
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z08622982W6HMKSZCC7DQ"
+  name    = "mongodb-dev.rdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
 
 resource "aws_instance" "catalogue" {
-  ami           = "data.aws_ami.centos.image_id"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t2.small"
 
   tags = {
@@ -55,16 +55,16 @@ resource "aws_instance" "catalogue" {
   }
 }
 
-#resource "aws_route53_record" "catalogue" {
-#  zone_id = "Z08622982W6HMKSZCC7DQ"
-#  name    = "catalogue-dev.rdevopsb72.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.catalogue.private_ip]
-#}
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z08622982W6HMKSZCC7DQ"
+  name    = "catalogue-dev.rdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
 
 resource "aws_instance" "redis" {
-  ami           = "data.aws_ami.centos.image_id"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t2.small"
 
   tags = {
@@ -72,16 +72,16 @@ resource "aws_instance" "redis" {
   }
 }
 
-#resource "aws_route53_record" "redis" {
-#  zone_id = "Z08622982W6HMKSZCC7DQ"
-#  name    = "redis-dev.rdevopsb72.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.redis.private_ip]
-#}
+resource "aws_route53_record" "redis" {
+  zone_id = "Z08622982W6HMKSZCC7DQ"
+  name    = "redis-dev.rdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
 
 resource "aws_instance" "user" {
-  ami           = "data.aws_ami.centos.image_id"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t2.small"
 
   tags = {
@@ -89,16 +89,16 @@ resource "aws_instance" "user" {
   }
 }
 
-#resource "aws_route53_record" "user" {
-#  zone_id = "Z08622982W6HMKSZCC7DQ"
-#  name    = "user-dev.rdevopsb72.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.user.private_ip]
-#}
+resource "aws_route53_record" "user" {
+  zone_id = "Z08622982W6HMKSZCC7DQ"
+  name    = "user-dev.rdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
 
 resource "aws_instance" "cart" {
-  ami           = "data.aws_ami.centos.image_id"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t2.small"
 
   tags = {
@@ -106,16 +106,16 @@ resource "aws_instance" "cart" {
   }
 }
 
-#resource "aws_route53_record" "cart" {
-#  zone_id = "Z08622982W6HMKSZCC7DQ"
-#  name    = "cart-dev.rdevopsb72.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.cart.private_ip]
-#}
+resource "aws_route53_record" "cart" {
+  zone_id = "Z08622982W6HMKSZCC7DQ"
+  name    = "cart-dev.rdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
 
 resource "aws_instance" "mysql" {
-  ami           = "data.aws_ami.centos.image_id"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t2.small"
 
   tags = {
@@ -123,16 +123,16 @@ resource "aws_instance" "mysql" {
   }
 }
 
-#resource "aws_route53_record" "mysql" {
-#  zone_id = "Z08622982W6HMKSZCC7DQ"
-#  name    = "mysql-dev.rdevopsb72.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.mysql.private_ip]
-#}
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z08622982W6HMKSZCC7DQ"
+  name    = "mysql-dev.rdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
 
 resource "aws_instance" "shipping" {
-  ami           = "data.aws_ami.centos.image_id"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t2.small"
 
   tags = {
@@ -140,16 +140,16 @@ resource "aws_instance" "shipping" {
   }
 }
 
-#resource "aws_route53_record" "shipping" {
-#  zone_id = "Z08622982W6HMKSZCC7DQ"
-#  name    = "shipping-dev.rdevopsb72.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.shipping.private_ip]
-#}
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z08622982W6HMKSZCC7DQ"
+  name    = "shipping-dev.rdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
 
 resource "aws_instance" "rabbitmq" {
-  ami           = "data.aws_ami.centos.image_id"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t2.small"
 
   tags = {
@@ -157,16 +157,16 @@ resource "aws_instance" "rabbitmq" {
   }
 }
 
-#resource "aws_route53_record" "rabbitmq" {
-#  zone_id = "Z08622982W6HMKSZCC7DQ"
-#  name    = "rabbitmq-dev.rdevopsb72.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.rabbitmq.private_ip]
-#}
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z08622982W6HMKSZCC7DQ"
+  name    = "rabbitmq-dev.rdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
 
 resource "aws_instance" "payment" {
-  ami           = "data.aws_ami.centos.image_id"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t2.small"
 
   tags = {
@@ -174,16 +174,16 @@ resource "aws_instance" "payment" {
   }
 }
 
-#resource "aws_route53_record" "payment" {
-#  zone_id = "Z08622982W6HMKSZCC7DQ"
-#  name    = "payment-dev.rdevopsb72.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.payment.private_ip]
-#}
+resource "aws_route53_record" "payment" {
+  zone_id = "Z08622982W6HMKSZCC7DQ"
+  name    = "payment-dev.rdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
+}
 
 resource "aws_instance" "dispatch" {
-  ami           = "data.aws_ami.centos.image_id"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t2.small"
 
   tags = {
@@ -191,10 +191,10 @@ resource "aws_instance" "dispatch" {
   }
 }
 
-#resource "aws_route53_record" "dispatch" {
-#  zone_id = "Z08622982W6HMKSZCC7DQ"
-#  name    = "dispatch-dev.rdevopsb72.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.dispatch.private_ip]
-#}
+resource "aws_route53_record" "dispatch" {
+  zone_id = "Z08622982W6HMKSZCC7DQ"
+  name    = "dispatch-dev.rdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.dispatch.private_ip]
+}
