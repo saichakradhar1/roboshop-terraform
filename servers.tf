abb6fb237c65dcc5a4ba293fd7bdd38ceeae5ab2
saichakradhar <saichakradhar1427@gmail.com>
2023-07-07 14:03:57 +0530

    Creating environments

--- servers.tf
+++ servers.tf
@@ -1,65 +1,5 @@
-data "aws_ami" "centos" {
-  owners      = ["973714476881"]
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-}
-
-output "ami" {
-  value = data.aws_ami.centos.image_id
-}
-
-data "aws_security_group" "allow-all" {
-  name = "allow-all"
-}
-
-variable "components" {
-  default = {
-    frontend = {
-      name          = "frontend"
-      instance_type = "t3.small"
-    }
-    mongodb = {
-      name          = "mongodb"
-      instance_type = "t3.small"
-    }
-    catalogue = {
-      name          = "catalogue"
-      instance_type = "t3.small"
-    }
-    redis = {
-      name          = "redis"
-      instance_type = "t3.small"
-    }
-    user = {
-      name          = "user"
-      instance_type = "t3.small"
-    }
-    cart = {
-      name          = "cart"
-      instance_type = "t3.small"
-    }
-    mysql = {
-      name          = "mysql"
-      instance_type = "t3.small"
-    }
-    shipping = {
-      name          = "shipping"
-      instance_type = "t3.small"
-    }
-    rabbitmq = {
-      name          = "rabbitmq"
-      instance_type = "t3.small"
-    }
-    payment = {
-      name          = "payment"
-      instance_type = "t3.small"
-    }
-
-  }
-}
-
 resource "aws_instance" "instance" {
-  for_each = var.components
+  for_each               = var.components
   ami                    = data.aws_ami.centos.image_id
   instance_type          = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
@@ -71,10 +11,10 @@ resource "aws_instance" "instance" {
 
 
 resource "aws_route53_record" "records" {
-  for_each=var.components
-  zone_id = "Z08622982W6HMKSZCC7DQ"
-  name    = "${each.value["name"]}-dev.rdevopsb72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instance[each.value["name"]].private_ip]
+  for_each = var.components
+  zone_id  = "Z08622982W6HMKSZCC7DQ"
+  name     = "${each.value["name"]}-dev.rdevopsb72.online"
+  type     = "A"
+  ttl      = 30
+  records  = [aws_instance.instance[each.value["name"]].private_ip]
 }
