5308ebcf9a5afb28f0e057265e64ce16d9cf8afe
saichakradhar <saichakradhar1427@gmail.com>
2023-07-13 15:59:40 +0530

    provisioners

--- servers.tf
+++ servers.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "instance" {
 
 resource "null_resource" "provisioner" {
 
-  depends_on = [aws_instance.instance, aws_route53_record]
+  depends_on = [aws_instance.instance,aws_route53_record.records]
   for_each   = var.components
   provisioner "remote-exec" {
 
