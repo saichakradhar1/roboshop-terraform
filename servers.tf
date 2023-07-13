c24be491166dc11dcf6a4de8ced13bb603afc42b
saichakradhar <saichakradhar1427@gmail.com>
2023-07-13 15:52:33 +0530

    provisioners

--- servers.tf
+++ servers.tf
@@ -9,7 +9,27 @@ resource "aws_instance" "instance" {
   }
 }
 
+resource "null_resource" "provisioner" {
 
+  depends_on = [aws_instance.instance, aws_route53_record]
+  for_each   = var.components
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.instance[each.value["name"]].private_ip
+    }
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/saichakradhar1/roboshop-shell.git",
+      "cd roboshop-shell",
+      "sudo bash ${each.value["name"]}.sh"
+    ]
+  }
+
+}
 resource "aws_route53_record" "records" {
   for_each = var.components
   zone_id  = "Z08622982W6HMKSZCC7DQ"
